35414f6a18b00c8202734f992129a5f9cfbe047d
techwithtwin <realwebtechguru@gmail.com>
2024-12-11 14:38:21 +0300

    mounting the router

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import router from "./router";
 
 const app = express();
 
@@ -10,5 +11,7 @@ app.get("/", (req, res) => {
     message: "Hello",
   });
 });
+
+app.use("/api", router);
 
 export default app;
